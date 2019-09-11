0a0590d8d9c8692b6e93aa8a140bce74837aee0d
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-09-11 17:15:53 +0300

    BoardDeletionTests BeforeClass BeforeMethod added deletionBoardTest completed

--- trello-web/src/test/java/com/trello/qa/BoardDeletionTests.java
+++ trello-web/src/test/java/com/trello/qa/BoardDeletionTests.java
@@ -2,37 +2,42 @@ package com.trello.qa;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BoardDeletionTests extends TestBase{
 
+    @BeforeClass
+    public void ensurePreconditionsLogin(){
+        if(!isUserLoggedIn()){
+            login("m.duksaite@gmail.com","trusty07");
+        }
+    }
+
+    @BeforeMethod
+    public void isOnHomePage () {
+        if (!isTherePersonalBoards())
+        {
+            returnToHomePage();
+        }
+    }
+
     @Test
-    public void deletionBoardTest (){
-
-    int before = getPersonalBoardsCount();
-
-    clickOnFirstPrivateBoard();
-    clickOnMoreButtonInBoardMenu();
-   // initCloseBoard(); // it's simple click on link Close board
-
-    int after = getPersonalBoardsCount();
-
+    public void deletionBoardTest () {
+        int before = getPersonalBoardsCount();
+        clickOnFirstPrivateBoard();
+        clickOnMoreButtonInBoardMenu();
+        initCloseBoard();
+        confirmCloseBoard();
+        initPermanentlyDeleteBoard();
+        confirmDeleteBoard();
+        returnToHomePage();
+        int after = getPersonalBoardsCount();
+        Assert.assertEquals(after, before-1);
     }
 
-    public void clickOnMoreButtonInBoardMenu() {
-            WebElement menuButton = driver.findElement(By.cssSelector(".board-header-btn.mod-show-menu"));
-            System.out.println(menuButton.getCssValue("visibility"));
-        //if(menuButton.getAttribute("class").equals(""to write here what's in "" in that class"")
-            if(menuButton.getCssValue("visibility").equals("visible")){
-                click(By.cssSelector(".mod-show-menu"));
-                click(By.cssSelector(".js-open-more"));
-            } else{
-                click(By.cssSelector(".js-open-more"));
-            }
-    }
-
-    public void clickOnFirstPrivateBoard() {
-       click(By.xpath("//div[@class='boards-page-board-section mod-no-sidebar']//h3[contains(text(),'Personal Boards')]/ancestor::div[@class='boards-page-board-section mod-no-sidebar']//ul[@class='boards-page-board-section-list']//li"));
-    }
 
 }
+

6c34f3b7d0c1fd73c63f4045f202badf1f13f832
MaryDuk <m.duksaite@gmail.com>
2019-09-12 20:57:49 +0300

    BoardCreationTests @AfterClass deletBoardsInCycle needs to be checked, app.

--- trello-web/src/test/java/com/trello/qa/BoardCreationTests.java
+++ trello-web/src/test/java/com/trello/qa/BoardCreationTests.java
@@ -1,8 +1,6 @@
 package com.trello.qa;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,48 +9,48 @@ public class BoardCreationTests extends TestBase{
 
     @BeforeClass
     public void ensurePreconditionsLogin(){
-        if(!isUserLoggedIn()){
-            login("m.duksaite@gmail.com","trusty07");
+        if(!app.isUserLoggedIn()){
+            app.login("m.duksaite@gmail.com","trusty07");
         }
     }
 
     @BeforeMethod
     public void isOnHomePage () {
-        if (!isTherePersonalBoards())
+        if (!app.isTherePersonalBoards())
         {
-            returnToHomePage();
+            app.returnToHomePage();
         }
     }
 
     @Test
     public void testBoardCreationByClickingOnCreateNewBoardButton() throws InterruptedException {
         //Assert.assertTrue(isUserLoggedIn());
-        int before = getPersonalBoardsCount();
-        createBoardGreyButton("First Board");
-        confirmBoardCreationByClickingOnCreateNewBoardButton();
-        returnToHomePage();
-        int after = getPersonalBoardsCount();
+        int before = app.getPersonalBoardsCount();
+        app.createBoardGreyButton("First Board");
+        app.confirmBoardCreationByClickingOnCreateNewBoardButton();
+        app.returnToHomePage();
+        int after = app.getPersonalBoardsCount();
         Assert.assertEquals(after, before+1);
-        boolean isPresent = findWebElementByText("First Board");
+        boolean isPresent = app.findWebElementByText("First Board");
         Assert.assertEquals(isPresent, true);
     }
 
     @Test
     public void testBoardCreationByClickingOnPlusOnHeaderRight() {
-        int before = getPersonalBoardsCount();
-        clickOnPlusButtonOnHeader();
-        selectCreateBoardFromDropDown();
-        fillBoardCreationForm("qa21");
-        confirmBoardCreationByClickingOnPlusOnHeaderRight();
-        returnToHomePage();
-        int after = getPersonalBoardsCount();
+        int before = app.getPersonalBoardsCount();
+        app.clickOnPlusButtonOnHeader();
+        app.selectCreateBoardFromDropDown();
+        app.fillBoardCreationForm("qa21");
+        app.confirmBoardCreationByClickingOnPlusOnHeaderRight();
+        app.returnToHomePage();
+        int after = app.getPersonalBoardsCount();
         Assert.assertEquals(after, before+1);
-        boolean isPresent = findWebElementByText("qa21");
+        boolean isPresent = app.findWebElementByText("qa21");
         Assert.assertEquals(isPresent, true);
     }
-    @AfterClass
-    public void deleteExtraBoards (){
-        deleteBoardsInCycle();
-}
+//    @AfterClass
+//    public void deleteExtraBoards (){
+//        deleteBoardsInCycle();
+//}
 
 }

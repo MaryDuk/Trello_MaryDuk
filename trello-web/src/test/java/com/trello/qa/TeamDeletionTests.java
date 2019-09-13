7726d0e856dd1d5760867c359c589b90c765e931
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-09-13 09:12:18 +0300

    @Before\method isAnyTeamPresent added, 1 of 1 tests passed

--- trello-web/src/test/java/com/trello/qa/TeamDeletionTests.java
+++ trello-web/src/test/java/com/trello/qa/TeamDeletionTests.java
@@ -1,11 +1,39 @@
 package com.trello.qa;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TeamDeletionTests extends TestBase {
+    @BeforeClass
+    public void ensurePreconditionsLogin(){
+        if(!app.isUserLoggedIn()){
+            app.login("m.duksaite@gmail.com","trusty07");
+        }
+    }
+
+    @BeforeMethod
+    public void isOnHomePage () {
+        if (!app.isTherePersonalBoards())
+        {
+            app.returnToHomePage();
+        }
+    }
+
+    @BeforeMethod
+    public void isAnyTeamPresent (){
+        if (app.getTeamsCount()<1){
+            app.clickOnPlusButtonOnLeftNavMenu();
+            app.fillTeamCreationForm("Smile", "People");
+            app.clickContinueButton();
+            app.returnToHomePage();
+        }
+
+    }
+
     @Test
-    public void deleteTeamFromLeftNavMenu() {
+    public void deleteTeamFromLeftNavMenu(){
         int before = app.getTeamsCount();
         app.clickOnFirstTeam();
         app.openSettings();

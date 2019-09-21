e78ca26233952c9e25d287ecf894f3b226d04a14
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-09-21 20:28:27 +0300

    BoardModification class with test created

--- trello-web/src/test/java/com/trello/qa/tests/BoardModificationTests.java
+++ trello-web/src/test/java/com/trello/qa/tests/BoardModificationTests.java
@@ -1,5 +1,48 @@
 package com.trello.qa.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class BoardModificationTests extends TestBase {
+
+    @BeforeClass
+    public void ensurePreconditionsLogin(){
+        if(!app.getSessionHelper().isUserLoggedIn()){
+            app.getSessionHelper().login("m.duksaite@gmail.com","trusty07");
+        }
+    }
+
+    @BeforeMethod
+    public void isOnHomePage () {
+        if (!app.getBoardHelper().isTherePersonalBoards())
+        {
+            app.getBoardHelper().returnToHomePage();
+        }
+    }
+
+    @BeforeMethod
+    public void isAnyPersonalBoardPresent (){
+        if (app.getBoardHelper().getPersonalBoardsCount()<1) { app.getBoardHelper().clickOnPlusButtonOnHeader();
+            app.getBoardHelper().selectCreateBoardFromDropDown();
+            app.getBoardHelper().fillBoardCreationForm("Beautiful");
+            app.getBoardHelper().confirmBoardCreationByClickingOnPlusOnHeaderRight();
+            app.getBoardHelper().returnToHomePage();
+            app.getBoardHelper().refreshPage();
+
+        }
+    }
+
+  @Test
+  public void   testChangeBoardTitle () {
+        app.getBoardHelper().clickOnFirstPrivateBoard();
+        String name = "Great";
+        app.getBoardHelper().changeBoardName(name);
+        app.getBoardHelper().returnToHomePage();
+        app.getBoardHelper().refreshPage();
+        boolean isChangedBoardNamePresent = app.getBoardHelper().findWebElementByText(name);
+        Assert.assertEquals(isChangedBoardNamePresent, true);
+  }
 
 }

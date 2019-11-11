edbb65de700f32b659d7d796dc9a4c65ce9347ba
MaryDuk <m.duksaite@gmail.com>
2019-11-11 21:04:51 +0200

    BoardCreationTests clas added

---
+++ trello_mob/src/test/java/com/trello/qa/tests/BoardCreationTests.java
@@ -0,0 +1,16 @@
+package com.trello.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class BoardCreationTests extends TestBase {
+    @Test
+
+    public void testBoardCreation(){
+        app.getBoardHelper().clickOnPlusButton();
+
+    }
+
+
+}
+
+
